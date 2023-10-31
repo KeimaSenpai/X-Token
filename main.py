8e311fc37237a252845d818b0ecae5092c8ab3cb
KeimaSenpai <hg4052650@gmail.com>
2023-10-30 22:40:46 -0400

    Arreglos

--- main.py
+++ main.py
@@ -12,42 +12,41 @@ async def main(page: ft.Page):
     page.window_resizable = False
     page.padding = 0
 
-    # async def on_submit(e):
-    #     await extract_token(url, username, password)
+    async def on_submit(e):
+        await extract_token(url, username, password)
 
-    # async def extract_token(url, username, password):
-    #     # Hacer una solicitud HTTP a la página de inicio de sesión de Moodle
-    #     session = requests.Session()
-    #     login_url = url + '/login/index.php'
-    #     response = session.get(login_url)
+    async def extract_token(url, username, password):
+        # Hacer una solicitud HTTP a la página de inicio de sesión de Moodle
+        session = requests.Session()
+        login_url = url.value + '/login/index.php'
+        response = session.get(login_url)
 
-    #     # Buscar el token en la respuesta
-    #     token_pattern = r'name="logintoken" value="([^"]+)"'
-    #     match = re.search(token_pattern, response.text)
-    #     if not match:
-    #         raise ValueError(
-    #             'No se pudo encontrar el token de inicio de sesión')
-    #     token = match.group(1)
+        # Buscar el token en la respuesta
+        token_pattern = r'name="logintoken" value="([^"]+)"'
+        match = re.search(token_pattern, response.text)
+        if not match:
+            raise ValueError(
+                'No se pudo encontrar el token de inicio de sesión')
+        token = match.group(1)
 
-    #     # Iniciar sesión en Moodle
-    #     data = {
-    #         'username': username,
-    #         'password': password,
-    #         'logintoken': token,
-    #     }
-    #     response = session.post(login_url, data=data)
+        # Iniciar sesión en Moodle
+        data = {
+            'username': username.value,
+            'password': password.value,
+            'logintoken': token,
+        }
+        response = session.post(login_url, data=data)
 
-    #     # Buscar el token de sesión en las cookies
-    #     if 'MoodleSession' not in session.cookies:
-    #         raise ValueError('No se pudo encontrar el token de sesión')
-    #     session_token = session.cookies['MoodleSession']
+        # Buscar el token de sesión en las cookies
+        if 'MoodleSession' not in session.cookies:
+            raise ValueError('No se pudo encontrar el token de sesión')
+        session_token = session.cookies['MoodleSession']
+        return session_token
 
-    # return await session_token
-
-    # async def save_token(token):
-    #     # Guardar el token en un archivo de texto
-    #     with open('token.txt', 'w') as f:
-    #         f.write(token)
+    async def save_token(token):
+        # Guardar el token en un archivo de texto
+        with open('token.txt', 'w') as f:
+            f.write(token)
 
     logo = ft.Image('bg.png', height=130)
     url = ft.TextField(label='URL Moodle', height=40,
@@ -57,7 +56,7 @@ async def main(page: ft.Page):
     password = ft.TextField(label='Contraseña', height=40,
                             text_size=15, border_color='#FF0066', border_radius=10)
     btn = ft.ElevatedButton(text='Generar', color='#D41872',
-                            bgcolor='#191919', data=page)
+                            bgcolor='#191919', on_click=on_submit, data=page)
 
     # Centro
     contenido = ft.Column([
